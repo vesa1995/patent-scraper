cdfb886706291facdffae16cfe0e5fa401b65474
vesa1995 <miloswesovic@gmail.com>
2021-10-20 14:32:32 +0200

    removed delay and timeout in patent service

--- src/service/patentService.js
+++ src/service/patentService.js
@@ -4,7 +4,7 @@ async function getPatentData(appNumber) {
     const appNumberToSearch = appNumber;
     const scrappedData = {};
     const initialPageUrl = 'https://portal.bpo.bg/web/guest/bpo_online/-/bpo/epo_patent-search'
-    let launchOptions = {headless: true, args: ['--start-fullscreen'], waitUntil: 'networkidle2'};
+    let launchOptions = {headless: true, /*args: ['--start-fullscreen'],*/ waitUntil: 'networkidle2'};
     let browser = await puppeteer.launch(launchOptions);
     let page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
@@ -18,100 +18,129 @@ async function getPatentData(appNumber) {
     await page.goto(initialPageUrl, {waitUntil: 'networkidle2'});
 
     // english button
-    const [languageChoser] = await page.$x('//*[@id="ctvk_null_null"]');
-    await languageChoser.click()
+    const [languageChooser] = await page.$x('//*[@id="ctvk_null_null"]');
+    await languageChooser.click()
     await page.deleteCookie();
-    await page.waitForTimeout(3000)
+    // await page.waitForTimeout(3000)
 
-    // advanced search button
-    await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:togle_link',
-        {visibe: true});
-    const [linkHtml] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:togle_link"]')
-    if (linkHtml) {
-        await linkHtml.click({delay: 500});
-    }
+    try {
+        // advanced search button
+        // await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:togle_link',
+        //     {visible: true});
+        // await page.waitForXPath('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:togle_link"]',
+        //     {visible: true});
+        await page.waitForNetworkIdle();
+        const [linkHtml] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:togle_link"]')
+        if (linkHtml) {
+            await linkHtml.click(); // {delay: 500}
+        }
+        // await page.evaluate(() => {
+        //     const xpath = '//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:togle_link"]';
+        //     const result = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+        //     const r = result.iterateNext();
+        //     r.click();
+        // });
 
-    // application number checkbox
-    await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:j_idt24');
-    const [checkbox] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:j_idt24"]')
-    if (checkbox) {
-        await checkbox.click();
-    }
+        // application number checkbox
+        await page.waitForNetworkIdle();
+        // await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:j_idt24');
+        const [checkbox] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:j_idt24"]')
+        if (checkbox) {
+            await checkbox.click();
+        }
 
-    // application number form
-    await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:app-num-panel');
-    await page.waitForTimeout(800).then(async () => { // todo read waitForTimeout docs
+        // application number form
+        await page.waitForNetworkIdle();
+        // await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:app-num-panel');
+        // await page.waitForTimeout(800).then(async () => {
+        //     // input field
+        //     const [textBox1] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:app-num-start_input"]');
+        //     await textBox1.focus();
+        //     await page.keyboard.type(appNumberToSearch);
+        //     // search button
+        //     const [button] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:submit_button"]')
+        //     await button.click({delay: 500})
+        //     // search result table
+        //     await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:table_result');
+        //     // eye button
+        //     const [eyeLink] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:table_result:0:j_idt365"]')
+        //     await eyeLink.click({delay: 500})
+        // });
         // input field
         const [textBox1] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:app-num-start_input"]');
         await textBox1.focus();
         await page.keyboard.type(appNumberToSearch);
         // search button
         const [button] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:submit_button"]')
-        await button.click({delay: 500})
+        await button.click() // {delay: 500}
         // search result table
-        await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:table_result');
+        await page.waitForNetworkIdle();
+        // await page.waitForSelector('#_bposervicesportlet_WAR_bposervicesportlet_\\:main_form\\:table_result');
         // eye button
         const [eyeLink] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:main_form:table_result:0:j_idt365"]')
-        await eyeLink.click({delay: 500})
-    });
+        await eyeLink.click() // {delay: 500}
 
-    await page.waitForNavigation();
+        // table page
+        await page.waitForNetworkIdle();
+        // await page.waitForNavigation();
+        // await page.waitForTimeout(2000)
 
-    await page.waitForTimeout(2000)
+        // open all button
+        const [expanderLink] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:j_idt13:j_idt23:open_all_toggel_panel"]');
+        await expanderLink.click() // {delay: 1000}
+        // details data
+        await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63', el => {
+            return el.outerHTML
+        });
+        // Details > Application number
+        const publicationReferenceId = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(1) > div:nth-child(2)', el => {
+            return el.textContent
+        })
+        // Details > Issue/Registration date
+        const registrationDate = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(2) > div:nth-child(4)', el => {
+            return el.textContent
+        })
+        // Details > Patent number todo maybe var name mistake
+        const applicationNumber = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(2) > div:nth-child(2)', el => {
+            return el.textContent
+        })
+        // Details > Application date
+        const applicationDate = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(1) > div:nth-child(4)', el => {
+            return el.textContent
+        })
+        // Details > Last paid
+        const lastPaid = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(5) > div:nth-child(4)', el => {
+            return el.textContent
+        })
+        // Details > Latest annual fee paid
+        const latestAnnualFeePayed = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(5) > div:nth-child(2)', el => {
+            return el.textContent
+        })
+        // Applicant/Owner
+        const applicationOwner = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt195 > div > div', el => {
+            return el.textContent
+        })
+        // Details > Status
+        const status = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(4) > div.ui-grid-col-9', el => {
+            return el.textContent
+        })
 
-    // open all button
-    const [expanderLink] = await page.$x('//*[@id="_bposervicesportlet_WAR_bposervicesportlet_:j_idt13:j_idt23:open_all_toggel_panel"]');
-    await expanderLink.click({delay: 1000})
-    // details data
-    await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63', el => {
-        return el.outerHTML
-    });
-    // Details > Application number
-    const publicationRefferenceId = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(1) > div:nth-child(2)', el => {
-        return el.textContent
-    })
-    // Details > Issue/Registration date
-    const registrationDate = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(2) > div:nth-child(4)', el => {
-        return el.textContent
-    })
-    // Details > Patent number todo maybe var name mistake
-    const applicationNumber = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(2) > div:nth-child(2)', el => {
-        return el.textContent
-    })
-    // Details > Application date
-    const applicationDate = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(1) > div:nth-child(4)', el => {
-        return el.textContent
-    })
-    // Details > Last paid
-    const lastPaid = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(5) > div:nth-child(4)', el => {
-        return el.textContent
-    })
-    // Details > Latest annual fee paid
-    const latestAnnualFeePayed = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(5) > div:nth-child(2)', el => {
-        return el.textContent
-    })
-    // Applicant/Owner
-    const applicationOwner = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt195 > div > div', el => {
-        return el.textContent
-    })
-    // Details > Status
-    const status = await page.$eval('#_bposervicesportlet_WAR_bposervicesportlet_\\:j_idt13\\:j_idt62\\:j_idt63 > div:nth-child(4) > div.ui-grid-col-9', el => {
-        return el.textContent
-    })
+        scrappedData.patentNumber = publicationReferenceId;
+        scrappedData.applicationNumber = applicationNumber; // todo mistake, see other todo comment
+        scrappedData.registrationDate = registrationDate;
+        scrappedData.aplicationDate = applicationDate;
+        scrappedData.lastPaid = lastPaid;
+        scrappedData.latestAnnualFeePayed = latestAnnualFeePayed;
+        scrappedData.aplicationOwner = applicationOwner;
+        scrappedData.status = status;
+        // todo add scrappedData.url (patent page url) - ovo moze biti da je nemoguce jer nema url,
+        //  vec po sesiji server zna sta nama treba
 
-    scrappedData.patentNumber = publicationRefferenceId;
-    scrappedData.applicationNumber = applicationNumber; // todo mistake, see other todo comment
-    scrappedData.registrationDate = registrationDate;
-    scrappedData.aplicationDate = applicationDate;
-    scrappedData.lastPaid = lastPaid;
-    scrappedData.latestAnnualFeePayed = latestAnnualFeePayed;
-    scrappedData.aplicationOwner = applicationOwner;
-    scrappedData.status = status;
-    // todo add scrappedData.url (patent page url) - ovo moze biti da je nemoguce jer nema url,
-    //  vec po sesiji server zna sta nama treba
-
-    console.log('#######', scrappedData);
-    await browser.close();
+        console.log('#######', scrappedData);
+        await browser.close();
+    } catch (err) {
+        console.log(err);
+    }
     return scrappedData;
 }
 
