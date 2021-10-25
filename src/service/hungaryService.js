facb4a307b2ab8e3229c86c45b280fcd54664534
vesa1995 <miloswesovic@gmail.com>
2021-10-25 10:49:07 +0200

    maintenance fees data is fixed

--- src/service/hungaryService.js
+++ src/service/hungaryService.js
@@ -19,16 +19,13 @@ async function scrape(appNumberToSearch) {
     await page.setViewport({width: 1440, height: 768});
     await page.goto(bulgariaPatentsPageURL, {waitUntil: 'networkidle2'});
 
-    const names = ['Luke', 'Eva', 'Phil']; // todo del
-    const [first, second] = names;
-    console.log(first, second); // 'Luke Eva'
-
     scrappedData.applicationNumber = appNumberToSearch;
     scrappedData.registrationNumber = "";
     scrappedData.aplicationDate = "";
-    scrappedData.status = "";
+    scrappedData.status = [];
     scrappedData.registrationDate = "";
     scrappedData.applicationOwner = "";
+    scrappedData.maintenanceFees = {};
 
     try {
         // input field
@@ -44,13 +41,11 @@ async function scrape(appNumberToSearch) {
         return scrappedData;
     }
 
-    let selector = '.ev_dhx_skyblue > td:nth-child(4)';
     // table > registration number
-    scrappedData.registrationNumber = await getContent(selector);
+    scrappedData.registrationNumber = await getContent('.ev_dhx_skyblue > td:nth-child(4)');
 
-    selector = '.ev_dhx_skyblue > td:nth-child(9)';
     // table > application date
-    scrappedData.aplicationDate = await getContent(selector);
+    scrappedData.aplicationDate = await getContent('.ev_dhx_skyblue > td:nth-child(9)');
 
     try {
         // app number > click
@@ -89,90 +84,80 @@ async function scrape(appNumberToSearch) {
         return scrappedData;
     }
 
-    const spans = await page.$$('.dhtmlx_wins_body_inner span'); // Array<ElementHandle>
-    console.log('spans[0].toString()', spans[0].toString());
-    const str = await spans[0].evaluate((node) => node.innerText);
-    console.log("str: ", str);
-    const str2 = await spans[2].evaluate((node) => node.textContent);
-    console.log("str2: ", str2);
-    let saveSpan;
-    for (let i = 1; i < spans.length; i++) {
-        let txt = await spans[i].evaluate((node) => node.textContent );
-        let topPix = await spans[i].evaluate((node) => node.offsetTop );
-        if (txt === 'Fenntartási díjak') {
-            saveSpan = topPix;
+    // maintenance fees parsing... from here to end
+    const spanElements = await page.$$('.dhtmlx_wins_body_inner span'); // Array<ElementHandle>
+    // console.log('spans[0].toString()', spans[0].toString());
+    // const str = await spans[0].evaluate((node) => node.innerText);
+    // console.log("str: ", str);
+    // const str2 = await spans[2].evaluate((node) => node.textContent);
+    // console.log("str2: ", str2);
+    let maintenanceFeesLabelPositionY;
+    for (let i = 1; i < spanElements.length; i++) {
+        let text = await spanElements[i].evaluate((node) => node.textContent);
+        let yPosition = await spanElements[i].evaluate(
+            (node) => node.getBoundingClientRect().top + document.documentElement.scrollTop
+        );
+        if (text === 'Fenntartási díjak') {
+            maintenanceFeesLabelPositionY = yPosition;
+            break;
         }
-        // console.log(txt + '       .');
     }
+    // console.log('top offset from window ' + maintenanceFeesLabelPositionY);
 
-    console.log('top offset from window ' + saveSpan);
-    const hrs = await page.$$('.dhtmlx_wins_body_inner hr'); // Array<ElementHandle>
-    let saveTopPix;
-    for (let i = 1; i < hrs.length; i++) {
-        let topPix = await hrs[i].evaluate((node) => node.offsetTop );
-        console.log(topPix);
-        if (topPix > saveSpan) {
-            saveTopPix = topPix;
+    const hrElements = await page.$$('.dhtmlx_wins_body_inner hr');
+    let hrPositionY;
+    for (let i = 1; i < hrElements.length; i++) {
+        let yPosition = await hrElements[i].evaluate(
+            (node) => node.getBoundingClientRect().top + document.documentElement.scrollTop
+        );
+        // console.log(topPix);
+        if (yPosition > maintenanceFeesLabelPositionY) {
+            hrPositionY = yPosition;
             break;
         }
     }
 
-    const tableElems = await page.$$('.dhtmlx_wins_body_inner table'); // Array<ElementHandle>
-    let tableTop;
-    let eleeement;
-    for (let i = 1; i < tableElems.length; i++) {
-        let topPix = await tableElems[i].evaluate((node) => node.offsetTop );
-        if (topPix > saveTopPix) {
+    const rowElements = await page.$$('.dhtmlx_wins_body_inner table');
+    let lastRowPositionY;
+    // let lastRowElement;
+    for (let i = 1; i < rowElements.length; i++) {
+        let yPosition = await rowElements[i].evaluate(
+            (node) => node.getBoundingClientRect().top + document.documentElement.scrollTop
+        );
+        if (yPosition > hrPositionY) {
             break;
         }
-        eleeement = tableElems[i];
-        tableTop = topPix;
-        console.log("TABLE: " + topPix);
+        lastRowPositionY = yPosition;
+        // lastRowElement = rowElements[i];
+        // console.log("TABLE: " + yPosition);
     }
-    console.log('konacno resenje: '  + eleeement);
-    await parseFees(eleeement);
+    // console.log('ceo red: '  + lastRowElement);
+
+    // const spanElements = await page.$$('.dhtmlx_wins_body_inner span');
+    // let rowSpanPositionY;
+    let lastRowObject = {};
+    let columnNameArray = ['Payer', 'Deposit', 'Amount', 'Validity', 'Remnant'];
+    let idx = 0;
+    for (let i = 1; i < spanElements.length; i++) {
+        let yPosition = await spanElements[i].evaluate(
+            (node) => node.getBoundingClientRect().top + document.documentElement.scrollTop
+        );
+        if (yPosition >= lastRowPositionY && yPosition <= hrPositionY) {
+            let text = await spanElements[i].evaluate((node) => node.textContent.trim());
+            if (text !== '') {
+                lastRowObject[columnNameArray[idx]] = text;
+                idx++;
+            }
+        }
+        // console.log(yPosition, lastRowPositionY, hrPositionY, await spanElements[i].evaluate((node) => node.textContent));
+        // rowSpanPositionY = yPosition;
+    }
+    // console.log('res = ' + columnNameArray);
+    scrappedData.maintenanceFees = lastRowObject;
 
     // e-register > Maintenance fees > zadnji red ima oba podatka valjda
     // scrappedData.lastPaid = lastPaid; // deposit kolona
     // scrappedData.latestAnnualFeePaid = latestAnnualFeePaid; // todo ne znam koja kolona, vrv mora da se racuna
-}
-
-async function parseFees(element) {
-    let elementsArray = await page.evaluateHandle((node) => node.children, element);
-    console.log(elementsArray.length, 'leeeeeeeeeeeeeeen');
-    console.log(getContent(elementsArray));
-    for (let i = 1; i <= elementsArray.length; i++) {
-        let topPix = await elementsArray[i].evaluate((node) => node.textContent );
-        console.log(topPix);
-    }
-    const names = [];
-    names.push(await (await element.getProperty('tagName')).jsonValue());
-    console.log('ovo radi???? ' , names);
-    // for (let i = 1; i <= names.length; i++) {
-    //     let gfg = await names[i].evaluate((node) => node.textContent );
-    //     console.log(gfg);
-    // }
-
-    let childrenCount;
-    try {
-        childrenCount = await page.$eval(elementsArray,
-            el => {
-                return el.children.length;
-            }
-        );
-    } catch (err) {
-        console.log(err);
-        return "";
-    }
-    let strArray = [];
-    for (let i = 1; i < childrenCount; i++) {
-        let child = await getContent(elementsArray + ' > td:nth-child(' + i + ')');
-        if (child !== "") {
-            strArray.push(child);
-        }
-    }
-    console.log(strArray);
-    return strArray;
 }
 
 async function parseStatus(selector) {
