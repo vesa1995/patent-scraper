6aff193f0a7f735f09a85f4ed8a401ae6bbb8f8b
vesa1995 <miloswesovic@gmail.com>
2021-11-30 22:23:23 +0100

    browser service fix and helper methods

--- src/service/browserService.js
+++ src/service/browserService.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const {fetchData} = require('./requestService');
 
-let browser, page;
+let browser = null, page;
 // cons surfSharkPath = 'C:\\Users\\milos\\AppData\\Local\\Google\\Chrome\\User Data\\Defa' +
 //     'ult\\Extensions\\ailoabdmgclmfmhdagmlohpjlbpffblp\\3.9.1_0';
 // const chromePath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
@@ -14,40 +14,73 @@ async function startPage() {
     //     `--disable-extensions-except=${surfSharkPath}`,
     //     `--load-extension=${surfSharkPath}`,
     // ]
-
-    let webSocketDebuggerUrl = await fetchData();
-
-    const connectOptions = {
-        // headless: false,
-        // args: args,
-        waitUntil: 'networkidle2',
-        browserWSEndpoint: webSocketDebuggerUrl,
-        // ignoreDefaultArgs: ["--enable-automation"],
-        // executablePath: chromePath
-    };
-    browser = await puppeteer.connect(connectOptions);
+    if (browser == null) {
+        let webSocketDebuggerUrl = await fetchData();
+        const connectOptions = {
+            // headless: false,
+            // args: args,
+            waitUntil: 'networkidle2',
+            browserWSEndpoint: webSocketDebuggerUrl,
+            // ignoreDefaultArgs: ["--enable-automation"],
+            // executablePath: chromePath
+        };
+        browser = await puppeteer.connect(connectOptions);
+    }
+    console.log((await browser.pages()).length, ' - tab number');
 
     page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
     page.on('console', ((msg) => {
         if (msg.text().indexOf('debug') !== -1) {
-            // console.log(msg.text())
+            console.log(msg.text())
         }
     }));
+
+    await page.setDefaultNavigationTimeout(0);
+    // todo move this code somewhere else, this is for bulgaria page only
     await page.deleteCookie({
         name : 'JSESSIONID',
         domain : 'portal.bpo.bg'
     });
 
+    // make puppeteer work while tab (page) is unfocused
+    const session = await page.target().createCDPSession();
+    await session.send('Page.enable');
+    await session.send('Page.setWebLifecycleState', {state: 'active'});
+
+    // thread test
+    // await setupPageHeartBit('page' + counter, page);
+    // counter++;
+
     return page;
+}
+let counter = 0;
+
+async function setupPageHeartBit(pageId, page) {
+    page.on('console', msg => console.log(`[${pageId}]: ${msg.text()}`));
+    await page.evaluate(() => {
+        let i = 0;
+        setInterval(() => {
+            console.log((++i) + ' heartbit');
+        }, 500);
+    });
 }
 
 async function closePage() {
+    await listPages();
     await page.close();
+    await listPages();
 }
 
 async function closeBrowser() {
     await browser.close();
+}
+
+async function listPages() {
+    let pages = await browser.pages();
+    console.log(pages.length);
+    for (let i = 0; i < pages.length; i++) {
+        console.log(pages[i].url());
+    }
 }
 
 module.exports = {
